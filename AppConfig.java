d9977de84da6d9f52a3646f1237905445c456679
soniamit2181-developer <soni.amit2181@gmail.com>
2026-08-01 16:10:58 +0530

    Add files via upload

---
+++ AppConfig.java
@@ -0,0 +1,21 @@
+package com.pooja.dev.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public String getConfig() {
+		return "Configuration for Spring Boot Application...";
+	}
+	
+	@PostConstruct
+	public void postmessage() {
+		System.out.println("Welcome to My Spring Boot Application. Enjoy Life!!!");
+	}
+
+}
